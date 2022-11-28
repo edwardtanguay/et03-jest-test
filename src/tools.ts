e71725f18dad53243b4111598069ee89aa21c20c
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-28 02:47:53 +0100

    changed 3 back to 2

--- src/tools.ts
+++ src/tools.ts
@@ -6,11 +6,11 @@
  * '[primary]'
  */
 export const surround = (text: string, twoLetters: string) => {
-	if (twoLetters.length != 3) {
+	if (twoLetters.length != 2) {
 		return text;
 	} else {
-		const first = twoLetters[0];
-		const second = twoLetters[1];
-		return first + text + second;
+	const first = twoLetters[0];
+	const second = twoLetters[1];
+	return first + text + second;
 	}
 }
